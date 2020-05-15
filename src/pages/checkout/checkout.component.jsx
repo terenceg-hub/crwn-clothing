35a424745cdae06879a3a7cc02e22a0711254d62
Terence Govender <terenceg@paycorp.co.za>
2020-05-15 16:07:23 +0200

    updating stripe integrations and updating our favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import { 
+    selectCartItems,
+    selectCartTotal 
+} from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
@@ -30,9 +34,13 @@ const CheckoutPage = ({cartItems,total}) => (
         {cartItems.map(cartItem => (
             <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
-        <div className='total'>
-            <span>Total: ${total}</span>
+        <div className='total'>Total: ${total}</div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/21 - CVV:123
         </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
